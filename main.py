3f850aa08507ff0bf39b1eaabfdb010476c036bd
alezyy <alezyy@yahoo.com>
2022-03-05 01:26:54 -0500

    add variable

--- main.py
+++ main.py
@@ -20,6 +20,9 @@
 import Adafruit_Python_BMP.Adafruit_BMP.BMP085 as BMP085
 
 import time
+import json
+import requests
+
 
 def setup():
 	print ('\n Barometer begins...')
@@ -29,10 +32,21 @@ def loop():
 		sensor = BMP085.BMP085()
 		temp = sensor.read_temperature()	# Read temperature to veriable temp
 		pressure = sensor.read_pressure()	# Read pressure to veriable pressure
+		tempo = '{0:0.2f} C'.format(temp)
+		pression = '{0:0.2f} Pa'.format(pressure)
+        
+	#temp = {0:0.2f} C.format(temp);
 
 		print ('')
-		print ('      Temperature = {0:0.2f} C'.format(temp))		# Print temperature
-		print ('      Pressure = {0:0.2f} Pa'.format(pressure))	# Print pressure
+		#print ('      Temperature = {0:0.2f} C'.format(temp))		# Print temperature
+		print ('      Temperature =', tempo)		# Print temperature
+		#print ('      Pressure = {0:0.2f} Pa'.format(pressure))	# Print pressure
+		print ('      Pressure =', pression)	# Print pressure
+		
+		#data['temperature'] = tempo
+		#data['pressure']    = sense.get_pressure()
+		
+		
 		time.sleep(1)
 		print ('')
 
